b34d09911237e9acb603255244a7d212b3e7c8ad
Petra Paulin <petra.oster.paulin@gmail.com>
2024-09-04 12:05:59 +0200

    improved css

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -17,18 +17,21 @@ const ProductPage = () => {
   }, []);
 
   return (
-    <div className="align-element py-20">
-      <div className="displayProduct grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="bg- bg-white py-12 px-4 sm:px-8 lg:px-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {products.map((product) => (
-          <div key={product.id} className=" border p-4 rounded shadow">
+          <div
+            key={product.id}
+            className="border p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+          >
             <img
               src={product.image}
               alt={product.title}
-              className="w-full h-48 object-cover mb-2"
+              className="w-full h-40 object-contain mb-4"
             />
-            <h2 className="text-lg font-semibold">{product.title}</h2>
-            <p className="text-gray-500">{product.price} SEK</p>
-            <p className="text-gray-500">{product.description}</p>
+            <h2 className="text-md font-semibold mb-2">{product.title}</h2>
+            <p className="text-gray-600 mb-2">{product.price} SEK</p>
+            <p className="text-gray-500 text-sm">{product.description}</p>
           </div>
         ))}
       </div>
