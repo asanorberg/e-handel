a1b44607dc4ff411a159be7620eac8a15044fdd0
Ulfflo <145294663+Ulfflo@users.noreply.github.com>
2024-09-05 11:42:36 +0200

    modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import CartItem from "./CartItem"; // Importera din modal-komponent
 
 const Header = () => {
-  return (
-    <header className="header">
-      <div className="logo">
-        <img
-          className="imglogo"
-          src="/src/assets/cf07e26a-9bb2-4dc2-b6f3-6ce127c50549.png"
-          alt=""
-        />
-        <h1>YourShop</h1>
-      </div>
-      <nav className="nav">
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
+  const [isCartOpen, setCartOpen] = useState(false);
 
+  const toggleCartModal = () => {
+    setCartOpen(!isCartOpen);
+  };
+
+  return (
+    <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
+      <div className="flex items-center">
+        <img
+          className="w-12 h-12"
+          src="/src/assets/cf07e26a-9bb2-4dc2-b6f3-6ce127c50549.png"
+          alt="logo"
+        />
+        <h1 className="ml-2 text-2xl font-bold">YourShop</h1>
+      </div>
+      <nav>
+        <ul className="flex space-x-4">
           <li>
-            <a href="/contact">Contact</a>
+            <a href="/" className="hover:text-gray-400">
+              Home
+            </a>
+          </li>
+          
+          <li>
+            <a href="/contact" className="hover:text-gray-400">
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
-      <div className="header-icons">
-        <a href="/search" className="icon">
+      <div className="flex space-x-4">
+        <a href="/search" className="hover:text-gray-400">
           <i className="fas fa-search"></i>
         </a>
-        <a href="/CartItem" className="icon">
+        <button className="hover:text-gray-400" onClick={toggleCartModal}>
           <i className="fas fa-shopping-cart"></i>
-        </a>
-        <a href="/profile" className="icon">
+        </button>
+        <a href="/profile" className="hover:text-gray-400">
           <i className="fas fa-user"></i>
         </a>
       </div>
+      {isCartOpen && <CartItem closeModal={toggleCartModal} />}{" "}
+      {/* Visa modalen om isCartOpen är true */}
     </header>
   );
 };
